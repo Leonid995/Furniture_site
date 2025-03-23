4dba2350320bf409886f0df920d14d9569c581e5
Leonid995 <167637023+Leonid995@users.noreply.github.com>
2025-03-23 12:10:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+document.getElementById("other").addEventListener("click", function () {
+    window.location.href = "https://www.ikea.com/"; 
+});
+document.getElementById("check-out").addEventListener("click", function () {
+    window.location.href = "https://www.ikea.com/global/en/newsroom/"; 
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("fullscreen-container").style.display = "none";
+
+
+    const productButtons = document.querySelectorAll(".product-button");
+
+    productButtons.forEach(button => {
+        button.addEventListener("click", function () {
+
+            const productDiv = this.closest(".product");
+            const imageSrc = productDiv.querySelector("img").src;
+
+            
+            document.getElementById("fullscreen-image").src = imageSrc;
+            document.getElementById("fullscreen-container").style.display = "flex";
+        });
+    });
+
+  
+    document.getElementById("close-btn").addEventListener("click", function () {
+        document.getElementById("fullscreen-container").style.display = "none";
+    });
+});
